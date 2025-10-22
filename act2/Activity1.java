dcd7906b433e2524c72f0dcd8bf220bd5a2b3ba4
ahmadamjad221-tiu <gemenipro434@gmail.com>
2025-10-22 12:30:52 +0300

    Add files via upload

---
+++ act2/Activity1.java
@@ -0,0 +1,11 @@
+package act2;
+public class Activity1 {
+    public static void main(String[] args) {
+        
+        classActivity1 ahmad=new classActivity1();
+ahmad.setAge(45);
+ahmad.setName("ahmad");
+ahmad.setCountry("kurdstan(not a contry yet!)");
+ahmad.displaydata();
+    }
+}
---
+++ act2/classActivity1.java
@@ -0,0 +1,35 @@
+package act2;
+public class classActivity1 {
+     private String Name;
+    private int Age;
+    private String Country;
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+    public void displaydata() {
+    System.out.println("Name: " + Name);
+    System.out.println("Age: " + Age);
+    System.out.println("Country: " + Country);
+} 
+}
