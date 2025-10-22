2427cb4533121056d1e539054ad88926ba9cbe94
ahmadamjad221-tiu <gemenipro434@gmail.com>
2025-10-22 12:32:53 +0300

    Add files via upload

---
+++ javaapplication1/JavaApplication1.java
@@ -0,0 +1,31 @@
+package javaapplication1;
+public class JavaApplication1 {
+
+    
+    public static void main(String[] args) {
+        
+        person a=new person();
+        a.SetAge(5);
+   
+        a.SetContry("iraq");
+        a.SetName("ahmad");
+            
+
+       
+        
+       
+        
+    }
+}
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
---
+++ javaapplication1/person.java
@@ -0,0 +1,35 @@
+package javaapplication1;
+
+public class person {
+    
+    private String Name;
+    private int Age;
+    private String Country;
+    
+    
+     public void SetAge(int age){
+    Age=age;
+    }
+     
+     public int getAge(){
+     return Age;
+     }
+     public void SetName(String name){
+    Name=name;
+    
+    }
+     
+     public String getName(){
+     return Name;
+     }
+     
+       public void SetContry(String country){
+    Country=country;
+     
+}
+
+ public String getCountry(){
+     return Country;
+     }
+
+}
